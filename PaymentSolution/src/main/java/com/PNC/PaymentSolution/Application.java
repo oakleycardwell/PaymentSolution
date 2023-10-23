695dcba73cd1999f24554bb5e9d979be3ccff3f4
Oakley Cardwell <oakleydc123@gmail.com>
2023-10-22 20:35:38 -0500

    Initial commit

---
+++ PaymentSolution/src/main/java/com/PNC/PaymentSolution/Application.java
@@ -0,0 +1,13 @@
+package com.PNC.PaymentSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSolutionApplication.class, args);
+	}
+
+}
---
+++ PaymentSolution/src/test/java/com/PNC/PaymentSolution/PaymentSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PNC.PaymentSolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
