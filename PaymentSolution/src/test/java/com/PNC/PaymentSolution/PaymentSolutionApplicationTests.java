c9a0f63b5875412f5c20ca81cbdf47003d458647
Oakley Cardwell <oakleydc123@gmail.com>
2023-10-22 21:27:01 -0500

    Initial commit

--- PaymentSolution/src/test/java/com/PNC/PaymentSolution/PaymentSolutionApplicationTests.java
+++ PaymentSolution/src/test/java/com/PNC/PaymentSolution/PaymentSolutionApplicationTests.java
@@ -1,13 +1,20 @@
 package com.PNC.PaymentSolution;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PaymentSolutionApplicationTests {
 
+	@Autowired
+	private Application application;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(application).isNotNull();
 	}
 
 }
